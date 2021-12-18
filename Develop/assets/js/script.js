88a084430d6bae184bf3523031dd22b2ccf4dbb5
Smith <carliesmith@amrock.com>
2021-12-18 12:42:26 -0500

    got initial prompts to work

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,33 +1,35 @@
-var passwordStart = window.prompt("Please click on the Generate Password button!");
-var passwordCharacters = window.prompt("Select your requirements.");
+console.log("hello");
+var Upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var Lower = "abcdefghijklmnopqrstuvwxyz"; 
 
 // prompt user to click on the writePassword button
+var passwordStart = window.alert("Please click on the Generate Password button!");
 
 
+//user clicks on the Generate Password button
 var writePassword = function ()  {
+  //prompt user to choose the password length, then capture that number and it will be
+  //referenced later to set the random number of characters generated
   var promptLength = window.prompt("Please choose a number between 8 and 128.");
-  if (promptLength >=8 && x <=128) {
+  if (promptLength >=8 && promptLength <=128) {
   console.log(promptLength);
   passwordCharacters();
 } else {
   window.alert("Try again! Please choose a number between 8 and 128.");
   writePassword();
 }
+//prompt the user to choose password criteria
+var passwordCharacters = function() {
+  var promptUppers = window.prompt("Would you like to include uppercase letters?");
 
-
-
-}
-
-  if (passwordLength >=8 || <=128 ) {
-    console.log(passwordLength);
-    passwordCharacters();
-  } else {
-    window.alert("Try again! Please choose a number between 8 and 128.");
-    startGenerator();
+  //if user wants uppercase then store that and include the Upper variable in the generation
+  if (promptUppers) {
+    console.log(promptUppers);
+    var storeUpper = document.querySelector("upper");
   }
-//
-
-
+  }
+  //if user does NOT want uppercase then only include the Lower array in the generate
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -40,7 +42,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-passwordStart();
--- README.md
+++ README.md
@@ -43,45 +43,8 @@ The URL of the GitHub repository. Give the repository a unique name and include 
 
 
 
+//this is what will create the actual password based on the criteria
+var actualPassword = function () {
+  var value = promptLength
 
-
-var writePassword = function ()  {
-  var promptLength = window.prompt("Please choose a number between 8 and 128.");
-  if (promptLength >=8 && x <=128) {
-  console.log(promptLength);
-  passwordCharacters();
-} else {
-  window.alert("Try again! Please choose a number between 8 and 128.");
-  writePassword();
-}
-
-
-
-}
-
-  if (passwordLength >=8 || <=128 ) {
-    console.log(passwordLength);
-    passwordCharacters();
-  } else {
-    window.alert("Try again! Please choose a number between 8 and 128.");
-    startGenerator();
-  }
-//
-
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-passwordStart();
+  
